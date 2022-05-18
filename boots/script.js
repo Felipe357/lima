67cbcad2ead3b4567ee871bbc4f5ec5a829cf700
felipe357 <felipeserra.100@gmail.com>
2022-05-18 11:28:55 -0200

    at

--- boots/script.js
+++ boots/script.js
@@ -1,11 +1,8 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
@@ -17,3 +14,13 @@
       }, false)
     })
   })()
+
+  const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
